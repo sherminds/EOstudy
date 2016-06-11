5f53a76fc462acaf1605ee71ca4e12b62ca9a515
Florence Debarre <flo@flokapik-2.local>
2016-06-11 10:15:06 +0200

    done script to download and list ads by month

--- Evoldir/README.md
+++ Evoldir/README.md
@@ -3,8 +3,14 @@
 
 type
 ```
-./getlist Type
+./getlist Month
 ```
-where `Type` is either `Conferences` or `WorkshopsCourses`. 
+where `Month` is the three letter version of the month (Jan, Fev, ...). 
+
+This (replace `Month` by the 3-letter month)
+- creates a `.csv` file `ads_Month.csv` listing all the ads, their date and type (Conferences/WorkshopsCourses).
+- downloads all the texts of the ads, stored in a folder `ads/Month/`.
+- copies the list of ads in the working directory, to later include it in the file with all data.
+
 
 
--- Evoldir/getlist.sh
+++ Evoldir/getlist.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
 
-curl "http://life.mcmaster.ca/cgi-bin/my_wrap/brian/evoldir/$1/" > $1.txt
+# Argument 1: month
+
+# Print error if no argument supplied
+if [[ $# -eq 0 ]] ; then
+  echo 'Error: You need to supply the month (Jan, Fev, ...) as argument'
+  exit 1
+fi
+
+mkdir "ads/${1}"
+
+for typeevent in "Conferences" "WorkshopsCourses"
+do
+  echo ================================== $typeevent ==================================
+  # Download the evoldir webpage
+  curl "http://life.mcmaster.ca/cgi-bin/my_wrap/brian/evoldir/${typeevent}/" > ${typeevent}.txt
+
+  # Extract information from the source code: take lines with ads and with month
+  grep "~brian/evoldir/.* $1 " ${typeevent}.txt > ${typeevent}_tmp.txt
+
+  # Reformat
+  # 1: remove end of link </a>
+  # 2: remove beginning of link ...">
+  # 3: remove spaces before month
+  # 4: remove hour at the end
+  # 5: reformat date so that they all have same length
+  # 6: add typeevent at the end of the line
+  sed -e 's/<\/a>//g' -e 's/^.*">//g' -e "s/ .*$1/, $1/g" -e 's/ [0-9]\{2\}:[0-9]\{2\}//g' -e "s/$1  /$1 0/g" -e "s/$/, ${typeevent}/g" ${typeevent}_tmp.txt > ${typeevent}_tmp2.txt
+
+  # Download the ads
+  cat ${typeevent}_tmp2.txt | cut -d, -f1 | while read line
+  do
+    echo $line
+curl "http://life.mcmaster.ca/cgi-bin/my_wrap/brian/evoldir/${typeevent}/${line}" > ads/${1}/${line}.txt
+  done
+done
+
+# Concatenate the files
+cat Conferences_tmp2.txt WorkshopsCourses_tmp2.txt > ads_$1.csv
+
+# Clean dir
+rm *tmp*.txt
+rm Conferences.txt
+rm WorkshopsCourses.txt
+
+# Copy the output file in the working directory, to later add them to the whole file
+cp ads_$1.csv ../TMP_ads_$1.csv
 
