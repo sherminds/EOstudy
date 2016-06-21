7333dec7c35765655898b422eafbe98839362f22
Flo <florence.debarre@normalesup.org>
2016-06-21 16:07:08 +0200

    script to send emails is ready

--- send-emails.R
+++ send-emails.R
@@ -14,10 +14,6 @@ if(length(args) == 0){
   }
 }
 
-# Load the xtable package (to export tables as html)
-# install.packages("xtable")
-require("xtable", quietly = TRUE)
-
 # Load the data
 datatab <- read.csv("dummydata.csv", stringsAsFactors = FALSE)
 
@@ -32,48 +28,52 @@ monthlines <- (months==themonth)
 subdata <- datatab[monthlines & datatab$Include, ]
 dim(subdata)
 
-# Create directory with month name
-system(paste("mkdir", themonth))
-
-generatepage <- function(i){
-  # Generate a personalized webpage for a given line of the table
-  # Column names in the table
-  ## Invited_nb	Invited_W	Instructor_nb	Instructor_W	Org_nb	Org_W	SciCom_nb	SciCom_W
+generateemail <- function(i){
+  # Generate a personalized email for a given line of the table
   v <- subdata[i,]
   ADNAME <- v$Ad_ID
   ADDATE <- v$Ad_date
   EVENTTYPE <- v$Type
-  EVOLDIR <- paste("http://life.mcmaster.ca/~brian/evoldir/", v$Ad_Type, "/", ADNAME, sep = "")
-  WEBSITE <- v$Website
-  NBW <- v$Invited_W
-  NBS <- v$Invited_nb
-  PC <- round(NBW/NBS* 100, digits=2)
   CONTACTNAME <- v$Org_name1
+  CONTACTEMAIL <- v$Org_email1
   QADDRESS <- paste("https://flodebarre.github.io/EOstudy/", themonth, "/", ADNAME, ".html", sep = "")
   EVENTSHORTNAME <- v$Short_name
   
-  # Prepare the table that will be printed
-  subcol <- v[, c("Invited_nb", "Invited_W", "Instructor_nb", "Instructor_W", "Org_nb", "Org_W", "SciCom_nb", "SciCom_W")]
-  # More explicit names
-  namsub <- c("#Invited", "#Invited_Women", "#Instructors", "#Instructor_Women", "#Organizers", "#Organizers_Women", "#ScientificCommittee", "#ScientificCommittee_Women")
-  # Only include the ones that are not NA:
-  colinclude <- c(!is.na(subcol))
-  thetab <- subcol[colinclude]
-  names(thetab) <- namsub[colinclude]
-  htmltab <- print(xtable(thetab, align = rep("l", length(thetab)+1)), include.rownames = FALSE, type = "html", comment = FALSE)
-  htmltab2 <- gsub("\n", " ", htmltab)
+  emailtxt <- "Dear Dr. XXNAME,
+
+As part of the Equal Opportunity Initiative of the European Society for Evolutionary Biology (ESEB), we are currently investigating the potential factors affecting the proportion of invited female speakers in scientific events advertised on the Evoldir mailing list.
   
-  # Edit index.html and save it under the ad's name
-  cmd <- paste("sed -e 's/XXADNAME/", ADNAME, "/g' -e 's/XXEVENTTYPE/", tolower(EVENTTYPE), "/g' -e 's,XXEVOLDIR,", EVOLDIR, ",g' -e 's,XXWEBSITE,", WEBSITE, ",g' -e 's/XXNBW/", NBW, "/g' -e 's/XXNBS/", NBS, "/g' -e 's/XXDATE/", ADDATE, "/g' -e 's/XXPC/", PC, "/g'  -e 's,XXTABLE,", htmltab2, ",g' -e 's,stylesheets/,../stylesheets/,g' -e 's,pics/,../pics/,g' < pagetemplate.html > ", themonth, "/", ADNAME, ".html", sep="")
-  system(cmd)
-  #-------------------------------------------
-  cmdemail <- paste("sed -e 's/XXADNAME/", ADNAME, "/g' -e 's/XXNAME/", CONTACTNAME, "/g' -e 's/XXEVENTTYPE/", tolower(EVENTTYPE), "/g' -e 's,XXEVOLDIR,", EVOLDIR, ",g' -e 's,XXWEBSITE,", WEBSITE, ",g' -e 's,XXADDRESS,", QADDRESS, ",g' -e 's/XXNBW/", NBW, "/g' -e 's/XXNBS/", NBS, "/g' -e 's/XXDATE/", ADDATE, "/g' -e 's/XXPC/", PC, "/g'  -e 's,XXTABLE,", htmltab2, ",g' -e 's,stylesheets/,../stylesheets/,g' -e 's,pics/,../pics/,g' < emailtemplate.txt > ", themonth, "/", ADNAME, "_email.txt", sep="")
+We saw the post about the XXEVENTTYPE that you organize, and we would like to ask if you could answer a very short questionnaire, available at 
+XXQADDRESS ; 
+filling it in should take no more than 2 minutes.
   
-  #  printf "From: EO study <eostudy.2017@gmail.com>\nTo: recipient@domain.com\nSubject: Subject\n\nBody\n" | /usr/sbin/sendmail -F "Sender Name" -f "sender@domain.com" "flo.debarre@gmail.com"
+We thank you in advance for your time.
+Please feel free to contact us should you have any question.
+  
+Best wishes,
+  
+Flo Debarre, Nicolas Rode, Shermin de Silva, Line Ugelvig.
+
+-------------------------------------------------
+ EO-study
+  website: http://flodebarre.github.io/EOstudy
+  email: eostudy.2017@gmail.com
+-------------------------------------------------"
+  
+ # Fill in the blanks
+  tmp <- gsub("XXNAME", CONTACTNAME, emailtxt)
+  tmp <- gsub("XXEVENTTYPE", tolower(EVENTTYPE), tmp)
+  tmp <- gsub("XXQADDRESS", QADDRESS, tmp)
+  wholeemail <- paste("Subject: Questions on ", EVENTSHORTNAME, "\n\n", tmp, "\n", sep="")
+  
+  # Write the whole command to send the email
+  cmdemail <- paste('printf "From: EO study <eostudy.2017@gmail.com>\nTo: ', CONTACTEMAIL, '\n', wholeemail, '" | /usr/sbin/sendmail -F "EO study" -f "eostudy.2017@gmail.com" "', CONTACTEMAIL, '"', sep = '')
+
+  system(cmdemail)
+
 }
 
-
-# Generate the pages!
-lapply(seq_along(subdata$Ad_ID), generatepage)
+# send the emails!
+lapply(seq_along(subdata$Ad_ID), generateemail)
 
 
