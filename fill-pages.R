3cf58b53bbda7447e2f052a6167a16fe2a946b13
Flo <florence.debarre@normalesup.org>
2016-06-21 18:56:06 +0200

    new webpages

--- fill-pages.R
+++ fill-pages.R
@@ -19,7 +19,7 @@ if(length(args) == 0){
 require("xtable", quietly = TRUE)
 
 # Load the data
-datatab <- read.csv("dummydata.csv", stringsAsFactors = FALSE)
+datatab <- read.csv("data.csv", stringsAsFactors = FALSE)
 
 # Get the month information from the Ad_date column
 getmonth <- function(str){
