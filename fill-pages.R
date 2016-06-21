e4983924ded9d1c3a6e8f7233e14f808f90fd613
Flo <florence.debarre@normalesup.org>
2016-06-21 18:46:21 +0200

    new webpages

--- fill-pages.R
+++ fill-pages.R
@@ -79,7 +79,7 @@ generatepage <- function(i){
 
   
   inputxt <- function(j){
-    paste('<input type="text" maxlength="4" name="', entries[j], '" id="', entries[j], '" value=', subcol[j],' placeholder=', subcol[j], '>', sep="")
+    paste('<input type="text" style="width: 10px;" name="', entries[j], '" id="', entries[j], '" value=', subcol[j],' placeholder=', subcol[j], '>', sep="")
   }
   newtab <- as.data.frame(matrix(unlist(lapply(seq_along(entries), inputxt)), nrow = 1))
 
