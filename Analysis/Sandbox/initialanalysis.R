9c387330bae7f64073e46b55439a9a6d379dd95a
Flo <florence.debarre@normalesup.org>
2016-09-03 19:05:38 +0200

    playing with the analysis

--- Analysis/Sandbox/initialanalysis.R
+++ Analysis/Sandbox/initialanalysis.R
@@ -1,46 +1,63 @@
 # Clean the memory
 rm(list=ls())
 
-# Load the answers
-tbl <- read.csv("Responses_20160717.csv", stringsAsFactors = FALSE)
-
-# Create columns with ratios
-tbl <- cbind(tbl, rInv = tbl$New_Invited_W / tbl$New_Invited_nb, #
-             rIns = tbl$New_Instructor_W / tbl$New_Instructor_nb, #
-             rOrg = tbl$New_Org_W / tbl$New_Org_nb)
-
-# Plots
-# Questions
-boxplot(rInv ~ Question.1, data = tbl, main="Q1: Aware?")
-boxplot(rInv ~ Question.2, data = tbl, main="Q2: Took gender into account?")
-boxplot(rInv ~ Question.3, data = tbl, main="Q3: Prescriptions?")
-# Organisers
-plot(tbl$rOrg, tbl$rInv, xlim = c(0, 1), ylim = c(0, 1))
-summary(lm(rInv~rOrg, data = tbl))
+do.old <- FALSE
+if(do.old){
+# Keeping this for the record, but Responses are put back into AllData, so no need to look at them independently. 
+  # Load the answers
+  tbl <- read.csv("Responses_20160717.csv", stringsAsFactors = FALSE)
+  
+  # Create columns with ratios
+  tbl <- cbind(tbl, rInv = tbl$New_Invited_W / tbl$New_Invited_nb, #
+               rIns = tbl$New_Instructor_W / tbl$New_Instructor_nb, #
+               rOrg = tbl$New_Org_W / tbl$New_Org_nb)
+  
+  # Plots
+  # Questions
+  boxplot(rInv ~ Question.1, data = tbl, main="Q1: Aware?")
+  boxplot(rInv ~ Question.2, data = tbl, main="Q2: Took gender into account?")
+  boxplot(rInv ~ Question.3, data = tbl, main="Q3: Prescriptions?")
+  # Organisers
+  plot(tbl$rOrg, tbl$rInv, xlim = c(0, 1), ylim = c(0, 1))
+  summary(lm(rInv~rOrg, data = tbl))
+}
 
 # All data
 ## NOTE: THIS IS EXTREMELY COARSE, NEED TO DISTINGUISH BETWEEN TYPES OF EVENTS
-odata <- read.csv("AllData_20160718.csv", stringsAsFactors = FALSE)
+# Load the data
+odata <- read.csv("AllData_20160903.csv", stringsAsFactors = FALSE)
 dim(odata)
+# Select only the ones that we include in the study (removing duplicates etc.)
 adata <- odata[odata$Include == TRUE,]
 dim(adata)
+
+# Adding columns corresponding to ratios
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# NOTE: need to use the corrected values if there has been a correction
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 adata <- cbind(adata, rInv = adata$Invited_W / adata$Invited_nb, rIns = adata$Instructor_W / adata$Instructor_nb, rOrg = as.numeric(adata$Org_W) / as.numeric(adata$Org_nb))
 
+# Look at proportion of invited women as function of proportion women organizers
 plot(adata$rOrg, adata$rInv, xlim = c(0, 1), ylim = c(0, 1), xlab = "Prop. W among organisers", ylab = "Prop. W among invited speakers", las = 1)
 aOI  <- lm(rInv~rOrg, data=adata)
 abline(aOI)
+summary(aOI)
 
 # Histogram of proportion of women
-hist(adata$rInv, xlim=c(0,1), breaks=seq(-0.025,1.0,by=0.05), main = "AllData, Prop invited women", xlab = "Prop. invited women")
+hist(adata$rInv, xlim=c(0,1), breaks=seq(-0.025,1.025,by=0.05), main = "AllData, Prop invited women", xlab = "Prop. invited women", axes = FALSE, col='grey')
+axis(1, at=seq(0, 1, by=0.1))
+axis(2, las=1)
+
+cat("========================================================================\n  QUESTION: WHAT DO WE DO WITH EVENTS WITH ONLY ONE PERSON INVITED?\n========================================================================\n")
 
 # Those who replied
-aa <- adata[!is.na(adata$Org_reply1), ]
+aa <- adata[!(adata$Timestamp==""), ]
 # Check consistency when merging data
 all(aa$Ad_ID == aa$Event.name)
 
 # Histogram of proportion of women
-hist(aa$rInv, xlim=c(0,1), breaks=seq(-0.025,1.0,by=0.05), main = "Only replies, Prop invited women", xlab = "Prop. invited women", col="red", freq=TRUE)
-hist(aa[aa$Question.2=="Yes",]$rInv, xlim=c(0,1), breaks=seq(-0.025,1.0,by=0.05), main = "Only replies, Prop invited women", xlab = "Prop. invited women", col="yellow", add=TRUE, freq=TRUE)
+hist(aa$rInv, xlim=c(0,1), breaks=seq(-0.025,1.025,by=0.05), main = "Only replies, Prop invited women", xlab = "Prop. invited women", col="red", freq=TRUE)
+hist(aa[aa$Question.2=="Yes",]$rInv, xlim=c(0,1), breaks=seq(-0.025,1.025,by=0.05), main = "Only replies, Prop invited women", xlab = "Prop. invited women", col="yellow", add=TRUE, freq=TRUE)
 
 # Check lines where people corrected the result
 checkno <- aa[aa$Question.0=="No",]
@@ -67,10 +84,21 @@ diffcols <- !( compcols(tmp[,3], tmp[,4]) & compcols(tmp[,5], tmp[,6]) & compcol
 tmp[diffcols & tmp$Question.0=="Yes",]
 
 # Effect of proportion of female speakers on whether organizers replied
-tmp.reply <- !is.na(adata$Org_reply1)
+tmp.reply <- ((adata$Org_reply1)|(adata$Org_reply2)) & (!is.na(adata$Org_reply1)|!is.na(adata$Org_reply2))
 boxplot(adata$rInv ~ tmp.reply, main="Replied?")
+summary(lm(adata$rInv ~ tmp.reply))
 
 adata[is.na(adata$Org_reply1),]
 
 adata$Org_name1[duplicated(adata$Org_name1)]
 
+# Create columns with ratios
+tbl <- cbind(aa, rInv = aa$New_Invited_W / aa$New_Invited_nb, #
+             rIns = adata$New_Instructor_W / aa$New_Instructor_nb, #
+             rOrg = aa$New_Org_W / aa$New_Org_nb)
+
+# Plots
+# Questions
+boxplot(rInv ~ Question.1, data = tbl, main="Q1: Aware?")
+boxplot(rInv ~ Question.2, data = tbl, main="Q2: Took gender into account?")
+boxplot(rInv ~ Question.3, data = tbl, main="Q3: Prescriptions?")
